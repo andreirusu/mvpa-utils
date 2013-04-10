28c51fb780c9c297f21715eb976ef0b21376e108
Andrei Rusu <andreirusu@deepmind.com>
2013-04-10 19:36:12 +0100

    Per subject realignment.

--- functional_all_subjects.m
+++ functional_all_subjects.m
@@ -1,32 +1,21 @@
 %% GET ALL THE PROC directories with functional volumes coming from sessions.
 
+
 celldisp(cellstr(EXPERIMENT_DIR));
 
+% get all subject dirs
+dirs = dir('./s*');
+
 
 % List of open inputs
 % Named Directory Selector: Directory - cfg_files
-nrun = 1; % enter the number of runs here
-jobfile = {'/Users/andreirusu/mvpa/mvpa-utils/functional_all_subjects_job.m'};
+nrun = size(dirs,1); % enter the number of runs here
+jobfile = {strcat(CODE_PATH, '/functional_template_job.m')};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(1, nrun);
 for crun = 1:nrun
-    inputs{1, crun} = cellstr(EXPERIMENT_DIR); % Named Directory Selector: Directory - cfg_files
-end
-spm('defaults', 'FMRI');
-spm_jobman('serial', jobs, '', inputs{:});
-
-celldisp(sessionPROCPaths);
-
-
-%% PROCESS FUNCTIONAL VOLUMES IN THOSE FOLDERS
-% List of open inputs
-% Named Directory Selector: Directory - cfg_files
-nrun = size(sessionPROCPaths, 1); % enter the number of runs here
-jobfile = {'/Users/andreirusu/mvpa/mvpa-utils/functional_template_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(1, nrun);
-for crun = 1:nrun
-    inputs{1, crun} = cellstr(sessionPROCPaths{crun}); % Named Directory Selector: Directory - cfg_files
+    disp(dirs(crun).name)
+    inputs{1, crun} = cellstr(fullfile(pwd, dirs(1).name)); % Named Directory Selector: Directory - cfg_files
 end
 spm('defaults', 'FMRI');
 spm_jobman('serial', jobs, '', inputs{:});
