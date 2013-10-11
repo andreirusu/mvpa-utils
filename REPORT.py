f65b9003fe4361b9f4f6b06a41231d3acd134759
Andrei Rusu <andreirusu@deepmind.com>
2013-10-11 09:41:37 +0100

    Computing class specific statistics

--- REPORT.py
+++ REPORT.py
@@ -28,11 +28,20 @@ from ROIinfo import *
 def group_test_mean(preds, task1, task2, roi, hem) :
     print 'task1: ', task1
     print 'task2: ', task2
+    t = [ st.ttest_ind(preds[task1][s][roi][hem]['probs'],
+                       preds[task2][s][roi][hem]['probs'], equal_var=False)[0].item()  
+                for s in preds[task1]['subjects'] 
+                    if s in preds[task1] 
+                        and roi in preds[task1][s] 
+                        and SUBJECT_GROUP[int(re.findall(r'\d+', s)[0])] == 2 ]
+    """
     t = [ st.ttest_ind((preds[task1][s][roi][hem]['probs']) if  SUBJECT_GROUP[int(re.findall(r'\d+', s)[0])] == 2 else (1 - preds[task1][s][roi][hem]['probs'])  ,
                         (preds[task2][s][roi][hem]['probs']) if  SUBJECT_GROUP[int(re.findall(r'\d+', s)[0])] == 2 else (1 - preds[task2][s][roi][hem]['probs']), equal_var=False)[0].item()  
                 for s in preds[task1]['subjects'] 
                     if s in preds[task1] 
                         and roi in preds[task1][s] ]
+    
+    """
     tval, pval = st.ttest_1samp(t, 0)
     print 'T: '+ str(t)
     print 'Sample size: ' + str(np.size(t))
